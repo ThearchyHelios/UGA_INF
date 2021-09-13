50a8de990b65834209e89387a55775ea18b0a544
Marshellson <jiangyilun2000@outlook.com>
2021-09-13 22:39:12 +0200

    First commit

---
+++ INF101/1.1.3.py
@@ -0,0 +1,6 @@
+x = float(input("Saissez un reel"))
+y = float(input("Saissez un autre reel"))
+tem_ajouter = x + y
+tem_multiplier = x * y
+print(x , " + " , y, " = ", tem_ajouter)
+print(x , " * " , y, " = ", tem_multiplier)
