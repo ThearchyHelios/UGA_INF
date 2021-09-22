c8e6514280c31b6f164cce57bccf09d4d2fef091
Marshellson <jiangyilun2000@icloud.com>
2021-09-22 14:59:27 +0200

    Update 2.2.2.py

--- INF101/TP/TP1/2.2.2.py
+++ INF101/TP/TP1/2.2.2.py
@@ -5,17 +5,32 @@ nombre_correct = random.randint(0, 100)
 nombre_temp_gauche = 0
 nombre_temp_droite = 100
 
+chance_rest = 5
+chance_utilise = 0
+
 corrrect = False
+
 
 while not corrrect:
     if nombre_saisser > nombre_correct:
+        chance_rest -= 1
+        chance_utilise += 1
+        if chance_rest == 0:
+            print("Vous n'avez pas le chance!")
+            break
         nombre_temp_droite = nombre_saisser
-        print("Trop grand")
+        print("Trop grand (Chance rest: %i)" % chance_rest)
         print("Devine mon nombre entre %i et %i" % (nombre_temp_gauche, nombre_temp_droite))
         nombre_saisser = int(input("Saissez un nombre SVP: "))
+
     elif nombre_saisser < nombre_correct:
+        chance_utilise += 1
+        chance_rest -= 1
+        if chance_rest == 0:
+            print("Vous n'avez pas le chance!")
+            break
         nombre_temp_gauche = nombre_saisser
-        print("Trop petit")
+        print("Trop petit (Chance rest: %i)" % chance_rest)
         print("Devine mon nombre entre %i et %i" % (nombre_temp_gauche, nombre_temp_droite))
         nombre_saisser = int(input("Saissez un nombre SVP: "))
     elif nombre_saisser == nombre_correct:
