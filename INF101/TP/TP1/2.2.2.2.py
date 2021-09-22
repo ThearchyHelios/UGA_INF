c05b50935344a9f3ad6a1169b98030730a183e10
Marshellson <jiangyilun2000@icloud.com>
2021-09-22 22:40:54 +0200

    Update 2.2.2.2.py

--- INF101/TP/TP1/2.2.2.2.py
+++ INF101/TP/TP1/2.2.2.2.py
@@ -4,16 +4,20 @@ fois_jouer = int(input("Combien de fois tu vous jouer?"))
 
 for i in range(0, fois_jouer):
     nombre_random = random.randint(0, 100)
+    print("Le nombre correst est %s" % nombre_random)
     nombre_temp_gauche = 0
     nombre_temp_droite = 100
     while True:
         nombre_temp_random = random.randint(nombre_temp_gauche, nombre_temp_droite)
+        print("Je choisi %s" % nombre_temp_random)
         if nombre_temp_random < nombre_random:
+            print("Cest trop petit")
             nombre_temp_gauche = nombre_temp_random + 1
-            print("Je vais essayer %s a %s" % (nombre_temp_gauche, nombre_temp_droite))
+            print("Je vais essayer %s a %s\n" % (nombre_temp_gauche, nombre_temp_droite))
         elif nombre_temp_random > nombre_random:
+            print("Cest trop grand")
             nombre_temp_droite = nombre_temp_random - 1
-            print("Je vais essayer %s a %s" % (nombre_temp_gauche, nombre_temp_droite))
+            print("Je vais essayer %s a %s\n" % (nombre_temp_gauche, nombre_temp_droite))
         else:
             print("Yea, le bon reponse est: %s" % nombre_temp_random)
             print("---------------------------------")
