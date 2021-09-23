f549ecbcde3723889699847c9fac9c87c0117664
Marshellson <jiangyilun2000@icloud.com>
2021-09-23 13:30:03 +0200

    Update 2.2.2.2.py

--- INF101/TP/TP1/2.2.2.2.py
+++ INF101/TP/TP1/2.2.2.2.py
@@ -1,31 +1,41 @@
 import random
+import matplotlib.pyplot as plt
 
 fois_jouer = int(input("Combien de fois tu vous jouer?"))
-nombre_fois_utilise = 0
+nombre_fois_utilise_total = 0
 nombre_maximeme = 100
+fois_count = []
 
 for i in range(0, fois_jouer):
     nombre_random = random.randint(0, nombre_maximeme)
     print("Le nombre correst est %s" % nombre_random)
     nombre_temp_gauche = 0
     nombre_temp_droite = nombre_maximeme
+    nombre_fois_utilise_une_fois =0
     while True:
         nombre_temp_random = random.randint(nombre_temp_gauche, nombre_temp_droite)
         print("Je choisi %s" % nombre_temp_random)
         if nombre_temp_random < nombre_random:
             print("Cest trop petit")
-            nombre_fois_utilise += 1
+            nombre_fois_utilise_une_fois += 1
             nombre_temp_gauche = nombre_temp_random + 1
             print("Je vais essayer %s a %s\n" % (nombre_temp_gauche, nombre_temp_droite))
         elif nombre_temp_random > nombre_random:
             print("Cest trop grand")
-            nombre_fois_utilise += 1
+            nombre_fois_utilise_une_fois += 1
             nombre_temp_droite = nombre_temp_random - 1
             print("Je vais essayer %s a %s\n" % (nombre_temp_gauche, nombre_temp_droite))
         else:
             print("Yea, le bon reponse est: %s" % nombre_temp_random)
-            nombre_fois_utilise += 1
+            nombre_fois_utilise_une_fois += 1
+            nombre_fois_utilise_total += nombre_fois_utilise_une_fois
             print("---------------------------------")
+            fois_count.append(nombre_fois_utilise_une_fois)
             break
 
-print("En moyenne il utilise %s fois chaque question" % (nombre_fois_utilise/fois_jouer))
+print("Il utilise %s fois en %s.\nEn moyenne il utilise %s fois chaque question" % (
+    nombre_fois_utilise_total, fois_jouer, nombre_fois_utilise_total / fois_jouer))
+print(fois_count)
+
+plt.plot(fois_count)
+plt.show()
