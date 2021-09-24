74ea989fc6f8097c0fcdd24fda9e719b43639b06
Marshellson <jiangyilun2000@icloud.com>
2021-09-24 19:37:03 +0200

    Commit MultiProcessing.py

--- INF101/TP/TP1/2.2.2.2.py
+++ INF101/TP/TP1/2.2.2.2.py
@@ -2,6 +2,7 @@ import random
 import matplotlib.pyplot as plt
 import time
 from collections import Counter
+import multiprocessing as mp
 
 fois_jouer = int(input("Combien de fois tu vous jouer?"))
 nombre_fois_utilise_total = 0
