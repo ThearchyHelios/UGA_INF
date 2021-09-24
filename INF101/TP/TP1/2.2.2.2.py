8345b198761c2c7fe569d8b59380fb97dc3ce993
Marshellson <jiangyilun2000@icloud.com>
2021-09-24 19:56:03 +0200

    Update 2.2.2.2.py Add multicore support

--- INF101/TP/TP1/2.2.2.2.py
+++ INF101/TP/TP1/2.2.2.2.py
@@ -5,49 +5,57 @@ from collections import Counter
 import multiprocessing as mp
 
 fois_jouer = int(input("Combien de fois tu vous jouer?"))
-nombre_fois_utilise_total = 0
-nombre_maximeme = 9999999999999
-fois_count = []
-start_time = time.time()
 
-for i in range(0, fois_jouer):
-    nombre_random = random.randint(0, nombre_maximeme)
-    print("Le nombre correst est %s" % nombre_random)
-    nombre_temp_gauche = 0
-    nombre_temp_droite = nombre_maximeme
-    nombre_fois_utilise_une_fois = 0
-    while True:
-        nombre_temp_random = random.randint(nombre_temp_gauche, nombre_temp_droite)
-        print("Je choisi %s" % nombre_temp_random)
-        if nombre_temp_random < nombre_random:
-            print("Cest trop petit")
-            nombre_fois_utilise_une_fois += 1
-            nombre_temp_gauche = nombre_temp_random + 1
-            print("Je vais essayer %s a %s\n" % (nombre_temp_gauche, nombre_temp_droite))
-        elif nombre_temp_random > nombre_random:
-            print("Cest trop grand")
-            nombre_fois_utilise_une_fois += 1
-            nombre_temp_droite = nombre_temp_random - 1
-            print("Je vais essayer %s a %s\n" % (nombre_temp_gauche, nombre_temp_droite))
-        else:
-            print("Yea, le bon reponse est: %s" % nombre_temp_random)
-            nombre_fois_utilise_une_fois += 1
-            nombre_fois_utilise_total += nombre_fois_utilise_une_fois
-            print("---------------------------------")
-            fois_count.append(nombre_fois_utilise_une_fois)
-            break
+num_cores = int(mp.cpu_count())
+pool = mp.Pool(num_cores)
 
-print("Il utilise %s fois en %s jeu(s).\nEn moyenne il utilise %s fois chaque jeu" % (
-    nombre_fois_utilise_total, fois_jouer, nombre_fois_utilise_total / fois_jouer))
-print(fois_count)
-print(time.time() - start_time, "seconds")
 
-plt.subplot(211)
-plt.plot(fois_count, "ro")
-plt.plot(fois_count)
+def jeu(a):
+    nombre_fois_utilise_total = 0
+    nombre_maximeme = 9999
+    fois_count = []
+    start_time = time.time()
+    for i in range(0, fois_jouer):
+        nombre_random = random.randint(0, nombre_maximeme)
+        print("Le nombre correst est %s" % nombre_random)
+        nombre_temp_gauche = 0
+        nombre_temp_droite = nombre_maximeme
+        nombre_fois_utilise_une_fois = 0
+        while True:
+            nombre_temp_random = random.randint(nombre_temp_gauche, nombre_temp_droite)
+            print("Je choisi %s" % nombre_temp_random)
+            if nombre_temp_random < nombre_random:
+                print("Cest trop petit")
+                nombre_fois_utilise_une_fois += 1
+                nombre_temp_gauche = nombre_temp_random + 1
+                print("Je vais essayer %s a %s\n" % (nombre_temp_gauche, nombre_temp_droite))
+            elif nombre_temp_random > nombre_random:
+                print("Cest trop grand")
+                nombre_fois_utilise_une_fois += 1
+                nombre_temp_droite = nombre_temp_random - 1
+                print("Je vais essayer %s a %s\n" % (nombre_temp_gauche, nombre_temp_droite))
+            else:
+                print("Yea, le bon reponse est: %s" % nombre_temp_random)
+                nombre_fois_utilise_une_fois += 1
+                nombre_fois_utilise_total += nombre_fois_utilise_une_fois
+                print("---------------------------------")
+                fois_count.append(nombre_fois_utilise_une_fois)
+                break
 
-plt.subplot(212)
-count = Counter(fois_count)
-plt.bar(count.keys(), count.values())
+    print("Il utilise %s fois en %s jeu(s).\nEn moyenne il utilise %s fois chaque jeu" % (
+        nombre_fois_utilise_total, fois_jouer, nombre_fois_utilise_total / fois_jouer))
+    print(fois_count)
+    print(time.time() - start_time, "seconds")
 
-plt.show()
+    plt.subplot(211)
+    plt.plot(fois_count, "ro")
+    plt.plot(fois_count)
+
+    plt.subplot(212)
+    count = Counter(fois_count)
+    plt.bar(count.keys(), count.values())
+
+    plt.show()
+
+
+result = pool.apply_async(jeu(fois_jouer))
