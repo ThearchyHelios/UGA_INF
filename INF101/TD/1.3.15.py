ec32d3c07cd7ff5dfe4f7ab60c1cf430a0c92c00
Marshellson <jiangyilun2000@icloud.com>
2021-09-28 08:19:35 +0200

    fix: Update 1.3.15.py

--- INF101/TD/1.3.15.py
+++ INF101/TD/1.3.15.py
@@ -1,4 +1,4 @@
-A = int(input("Entrer"))
+A = int(input("Entrer: "))
 
 n = 1
 Un = A
@@ -12,5 +12,19 @@ while Un != 1:
         Un = Un / 2
     else:
         Un = 3 * Un + 1
+    if Un > Un_max:
+        Un_max = Un
+
+    if Un < Un_prev:
+        count_descente += 1
+    else:
+        count_descente = 1
+    Un_prev = Un
+
+    if count_descente > count_descente_max:
+        count_descente_max = count_descente
+
+    print("Un a l'etape %i est egale a %i" % (n, Un))
     n += 1
+
 print("Un = 1 lorsque n eset egale a %i et la plus grand valeur atteint par la suite %i" % (n, Un_max))
