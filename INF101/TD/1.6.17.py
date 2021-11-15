19364d3774b98491dc6a011ddc888caea9515249
Marshellson <jiangyilun2000@icloud.com>
2021-11-15 20:46:05 +0100

    Commit 1.6.17.py

--- INF101/TD/1.6.17.py
+++ INF101/TD/1.6.17.py
@@ -8,40 +8,97 @@ def taille(a):
     return liste_taille
 
 
-def lire(a):
+def lire(n):
     liste = []
-    for i in range(a):
+    for i in range(n):
         mot = input("Tapez un mot : ")
         liste.append(mot)
     return liste
 
 
-def affiche(a):
-    liste_taille = taille(a)
+def affiche(liste):
+    liste_taille = taille(liste)
     count_taille = 0
     for i in liste_taille:
         count_taille += i
 
-    for j in range(len(a)):
-        print("Taille du mot %s : %s" % (a[j], liste_taille[j]))
+    for j in range(len(liste)):
+        print("Taille du mot %s : %s" % (liste[j], liste_taille[j]))
 
-    taille_moyenne = count_taille / len(a)
+    taille_moyenne = count_taille / len(liste)
     print("Taille moyenne: ", taille_moyenne)
     print("Mots plus longs que la moyenne: ", end="")
     count_liste = 0
     for k in liste_taille:
         if k > taille_moyenne:
-            print(a[count_liste], end="; ")
+            print(liste[count_liste] + ";", end="")
         count_liste += 1
+    print("")
 
-
-def nbocc(a, b):
+def nbocc(mot, car):
     count = 0
-    for i in a :
-        if i == b:
+    for i in mot:
+        if i == car:
             count += 1
     return count
 
 
-liste = ["adgwg", "wgwrgwgwrgw", "wgwr", "wyebfliwblwbflwb", "anloanfanofne"]
-affiche(liste)
+def compteCarac(liste, car):
+    count_total = 0
+    liste_contient_car = []
+    for mot in liste:
+        temp = nbocc(mot, car)
+        count_total += temp
+        if temp != 0:
+            print(mot)
+            liste_contient_car.append(mot)
+
+    if count_total != 0:
+        print("Le caractère %s apparaît %s fois." % (car, count_total))
+        return liste_contient_car
+    else:
+        print("Erreur la lettre %s n’est présente dans aucun des mots" % car)
+        return False
+
+
+def plusLongCarac(liste, car):
+    liste_contient_car = compteCarac(liste, car)
+    if not liste_contient_car:
+        return
+    nombre_carac_plus_grand = 0
+    liste_mot_carac_plus_grand = []
+    for mot in liste_contient_car:
+        count = nbocc(mot, car)
+        if count > nombre_carac_plus_grand:
+            nombre_carac_plus_grand = count
+    for mot in liste_contient_car:
+        count = nbocc(mot, car)
+        if count == nombre_carac_plus_grand:
+            liste_mot_carac_plus_grand.append(mot)
+    count_lettre_minimum = len(liste_mot_carac_plus_grand[0])
+    for mot in liste_mot_carac_plus_grand:
+        count_lettre = 0
+        for lettre in mot:
+            count_lettre += 1
+        if count_lettre < count_lettre_minimum:
+            count_lettre_minimum = count_lettre
+    for mot in liste_mot_carac_plus_grand:
+        count_lettre = 0
+        for lettre in mot:
+            count_lettre += 1
+        if count_lettre == count_lettre_minimum:
+            print("Le mot avec le plus caractere \"%s\" le plus court est : %s" % (car, mot))
+            break
+
+
+# liste = ["adgwg", "wgwrgwgwrgw", "wgwr", "wyebfliwblwbflwb", "anloanfanofne", "wwwww"]
+# plusLongCarac(liste, "w")
+
+if __name__ == '__main__':
+    n = int(input("Saissez un nombre de fois"))
+    liste = lire(n)
+    while True:
+        car = input("Saissez un caractere")
+        affiche(liste)
+        plusLongCarac(liste, car)
+        print("--------------------")
