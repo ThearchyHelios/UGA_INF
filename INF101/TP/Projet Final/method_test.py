23df6af62c6971bad53d62536bd4d946699ba49e
Marshellson <jiangyilun2000@icloud.com>
2021-11-20 19:25:45 +0100

    Commit

--- INF101/TP/Projet Final/method_test.py
+++ INF101/TP/Projet Final/method_test.py
@@ -1,3 +1,11 @@
+'''
+Author: JIANG Yilun
+Date: 2021-11-18 23:10:12
+LastEditTime: 2021-11-20 18:59:41
+LastEditors: JIANG Yilun
+Description: 
+FilePath: /INF_101/INF101/TP/Projet Final/method_test.py
+'''
 # import random
 # total = [10, 100, 1000, 10000, 100000, 1000000, 5000000, 10000000, 100000000]  #随机点数
 # for t in total:
@@ -10,16 +18,26 @@
 #             in_count += 1
 #     print(t,'个随机点时，π是：', 4*in_count/t)'
 
+import sys
+import gym
 import numpy as np
+from collections import defaultdict
 
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, Flatten, Dropout
+from plot_utils import plot_blackjack_values, plot_policy
 
-model = Sequential()
-model.add(Dense(16))
-model.add(Dense(128))
-model.add(Dense(32))
-model.add(Dense(8))
-model.add(Dense(1, activation='sigmoid'))
 
-model.compile(loss='binary_crossentropy', optimizer='sgd')
+env = gym.make('Blackjack-v0')
+
+print(env.observation_space)
+print(env.action_space)
+
+for i_episode in range(3):
+    state = env.reset()
+    while True:
+        print(state)
+        action = env.action_space.sample()
+        state, reward, done, info = env.step(action)
+        if done:
+            print('End game! Reward: ', reward)
+            print('You won :)\n') if reward > 0 else print('You lost :(\n')
+            break
