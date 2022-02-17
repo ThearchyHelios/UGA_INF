10e2abad8429c3b26124e99f90f2593d0dc6e6c3
Marshellson <jiangyilun2000@icloud.com>
2022-02-17 09:02:20 +0100

    TP4.ml

--- INF201/TP4.ml
+++ INF201/TP4.ml
@@ -1,1 +1,11 @@
-(*Exercice 2.7*)
+(*Exercice 2.11*)
+type jour =  int;;
+type heure = int;;
+type minute = int;;
+type seconde = int;;
+
+type duree = jour * heure * minute * seconde;;
+
+let div (n:int) (d:int): int*int =
+  n / d, n mod d;;
+
